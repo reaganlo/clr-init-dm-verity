1a6a61aa4e780a98cf55b927362a548d84658309
Reagan Lopez <reagan.lopez@intel.com>
2018-04-25 23:01:27 +0000

    Removed unnecessary files from initramfs Removed debug statements from init

--- initramfs.sh
+++ initramfs.sh
@@ -2,7 +2,7 @@
 
 echo $1
 rm -rf initramfs $1
-mkdir -p initramfs/{bin,dev,etc,lib64/haswell,usr/lib64/haswell,mnt/root,proc,root,sys,run}
+mkdir -p initramfs/{bin,dev,etc,lib64,usr/lib64,mnt/root,proc,root,sys,run}
 
 mixversion=$(cat mixversion)
 echo $mixversion
@@ -15,19 +15,11 @@ else
 fi
 echo $path
 
-cp -a $path/usr/lib64/{libpthread.so.?,librt.so.?,libacl.so.?,libattr.so.?,libcap.so.?,ld-linux-x86-64.so.?,libmount.so.?,libblkid.so.?,libuuid.so.?,libcryptsetup.so.?,libpopt.so.?,libdevmapper.so.?.??,libgcrypt.so.??,libudev.so.?,libgpg-error.so.?,ld-?.??.so,libacl.so.?.?.?,libattr.so.?.?.?,libblkid.so.?.?.?,libcap.so.?.??,libcryptsetup.so.?.?.?,libgcrypt.so.??.?.?,libgpg-error.so.?.??.?,libmount.so.?.?.?,libpopt.so.?.?.?,libpthread-?.??.so,librt-?.??.so,libudev.so.?.?.?,libuuid.so.?.?.?,libtinfo.so.?,libtinfo.so.?.?} initramfs/usr/lib64/
+cp -a $path/usr/lib64/{libc.so.?,libgcc_s.so.?,libc-?.??.so,libm.so.?,libm-?.??.so,libpthread.so.?,librt.so.?,libacl.so.?,libattr.so.?,libcap.so.?,ld-linux-x86-64.so.?,libmount.so.?,libblkid.so.?,libuuid.so.?,libcryptsetup.so.?,libpopt.so.?,libdevmapper.so.?.??,libgcrypt.so.??,libudev.so.?,libgpg-error.so.?,ld-?.??.so,libacl.so.?.?.?,libattr.so.?.?.?,libblkid.so.?.?.?,libcap.so.?.??,libcryptsetup.so.?.?.?,libgcrypt.so.??.?.?,libgpg-error.so.?.??.?,libmount.so.?.?.?,libpopt.so.?.?.?,libpthread-?.??.so,librt-?.??.so,libudev.so.?.?.?,libuuid.so.?.?.?,libtinfo.so.?,libtinfo.so.?.?} initramfs/usr/lib64/
 
-cp -a $path/usr/lib64/{libc.so.?,libgcc_s.so.?,libc-?.??.so} initramfs/usr/lib64/haswell/
+cp -a $path/usr/lib64/{libc.so.?,libgcc_s.so.?,libc-?.??.so,libm.so.?,libm-?.??.so,libpthread.so.?,librt.so.?,libacl.so.?,libattr.so.?,libcap.so.?,ld-linux-x86-64.so.?,libmount.so.?,libblkid.so.?,libuuid.so.?,libcryptsetup.so.?,libpopt.so.?,libdevmapper.so.?.??,libgcrypt.so.??,libudev.so.?,libgpg-error.so.?,ld-?.??.so,libacl.so.?.?.?,libattr.so.?.?.?,libblkid.so.?.?.?,libcap.so.?.??,libcryptsetup.so.?.?.?,libgcrypt.so.??.?.?,libgpg-error.so.?.??.?,libmount.so.?.?.?,libpopt.so.?.?.?,libpthread-?.??.so,librt-?.??.so,libudev.so.?.?.?,libuuid.so.?.?.?,libtinfo.so.?,libtinfo.so.?.?} initramfs/lib64/
 
-cp -a $path/usr/lib64/{haswell/libm.so.?,haswell/libm-?.??.so} initramfs/usr/lib64/haswell/
-
-cp -a $path/lib64/{libpthread.so.?,librt.so.?,libacl.so.?,libattr.so.?,libcap.so.?,ld-linux-x86-64.so.?,libmount.so.?,libblkid.so.?,libuuid.so.?,libcryptsetup.so.?,libpopt.so.?,libdevmapper.so.?.??,libgcrypt.so.??,libudev.so.?,libgpg-error.so.?,ld-?.??.so,libacl.so.?.?.?,libattr.so.?.?.?,libblkid.so.?.?.?,libcap.so.?.??,libcryptsetup.so.?.?.?,libgcrypt.so.??.?.?,libgpg-error.so.?.??.?,libmount.so.?.?.?,libpopt.so.?.?.?,libpthread-?.??.so,librt-?.??.so,libudev.so.?.?.?,libuuid.so.?.?.?,libtinfo.so.?,libtinfo.so.?.?} initramfs/lib64/
-
-cp -a $path/lib64/{libc.so.?,libgcc_s.so.?,libc-?.??.so} initramfs/lib64/haswell/
-
-cp -a $path/lib64/{haswell/libm.so.?,haswell/libm-?.??.so} initramfs/lib64/haswell/
-
-cp -a $path/bin/{bash,sh,coreutils,mount,umount,veritysetup,switch_root,cat,sleep,ls,echo} initramfs/bin/
+cp -a $path/bin/{bash,sh,coreutils,mount,umount,veritysetup,switch_root,cat,echo} initramfs/bin/
 
 cp init initramfs/
 
